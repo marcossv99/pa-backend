ba8e802f5f7bcedc5df932fb6c8c17f3ce1bc2e2
marcossv99 <marcoss.gwp@gmail.com>
2025-05-29 15:04:32 -0300

    conxao do backend local com docker com angular

--- src/main/java/com/example/back/controller/SocioController.java
+++ src/main/java/com/example/back/controller/SocioController.java
@@ -1,7 +1,6 @@
 package com.example.back.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.back.dto.SocioCadastroDTO;
 
-@RestController
-@RequestMapping("/api/socio")
-@CrossOrigin(origins = "*")
-public class UsuarioController {
+@RestController 
+@RequestMapping("/api/socios") // Mapeia a URL base para este controlador
+public class SocioController {
+
+    // Endpoint para cadastrar um novo associado
+    // O método recebe um objeto SocioCadastroDTO no corpo da requisição
     @PostMapping("/cadastro")
     public ResponseEntity<String> cadastrarSocio(@RequestBody SocioCadastroDTO socioCadastroDTO) {
-        // Aqui você pode implementar a lógica de cadastro, como salvar os dados em um banco de dados
+        // Aqui você pode implementar a lógica de cadastro, como salvar os dados em um
+        // banco de dados
         System.out.println("Cadastro de associado" + socioCadastroDTO);
         return ResponseEntity.ok("Associado realizado com sucesso!");
     }
